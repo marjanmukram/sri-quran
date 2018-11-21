cb456a19f04eb598c14fd527f7274d0489b9387a
Rizan Mohomed <musthafarizan@gmail.com>
2018-11-21 10:14:05 +0530

    intial setup with react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from './components/MainNavbar';
+
+class App extends Component {
+  render() {
+    return <div className="App">sri quran</div>;
+  }
+}
+
+export default App;
